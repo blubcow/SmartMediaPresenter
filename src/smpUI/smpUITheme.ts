a633337d59b6e1bb39cf68f32b2da58ab7986b7e
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-10-18 18:41:02 +0200

    added basic color palette for light and dark theme ;

---
+++ src/smpUI/smpUITheme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material';
+
+export const lightTheme = createTheme({
+	palette: {
+		primary: { main: '#1DA1F2' },
+		secondary: { main: '#405DE6' },
+		background: { default: '#F5F8FA', paper: '#AAB8C2' },
+		text: { primary: '#14171A', secondary: '#657786' },
+		success: { main: '#2EB67D' },
+		error: { main: '#E01E5A' },
+		warning: { main: '#ECB22E' },
+	},
+});
+
+export const darkTheme = createTheme({
+	palette: {
+		primary: { main: '#1DA1F2' },
+		secondary: { main: '#405DE6' },
+		background: { default: '#14171A', paper: '#657786' },
+		text: { primary: '#F5F8FA', secondary: '#AAB8C2' },
+		success: { main: '#2EB67D' },
+		error: { main: '#E01E5A' },
+		warning: { main: '#ECB22E' },
+	},
+});
