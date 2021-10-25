b5a25a10e63370a973116952ddb87a55f8ea2039
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-10-25 12:28:21 +0200

    updated Box smpUI component

--- src/smpUI/components/Box/index.tsx
+++ src/smpUI/components/Box/index.tsx
@@ -1,8 +1,10 @@
 import React, { PropsWithChildren } from 'react';
-import { Box as MUIBox } from '@mui/material';
+import { Box as MUIBox, BoxProps } from '@mui/material';
 
-interface IBoxProps {}
+interface IBoxProps extends BoxProps {}
 
 const Box: React.FC<PropsWithChildren<IBoxProps>> = (props) => {
 	return <MUIBox {...props}></MUIBox>;
 };
+
+export default Box;
