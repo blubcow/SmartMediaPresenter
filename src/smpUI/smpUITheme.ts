970a754120ce5dc98383942735d9c1a61d400f6d
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-10-28 21:43:44 +0200

    updated paper background color of light theme

--- src/smpUI/smpUITheme.ts
+++ src/smpUI/smpUITheme.ts
@@ -4,7 +4,7 @@ export const lightTheme = createTheme({
 	palette: {
 		primary: { main: '#1DA1F2' },
 		secondary: { main: '#405DE6' },
-		background: { default: '#F5F8FA', paper: '#AAB8C2' },
+		background: { default: '#F5F8FA', paper: '#D8E7EF' },
 		text: { primary: '#14171A', secondary: '#282E33' },
 		success: { main: '#2EB67D' },
 		error: { main: '#E01E5A' },
@@ -17,7 +17,7 @@ export const darkTheme = createTheme({
 		primary: { main: '#1DA1F2' },
 		secondary: { main: '#405DE6' },
 		background: { default: '#14171A', paper: '#282E33' },
-		text: { primary: '#F5F8FA', secondary: '#AAB8C2' },
+		text: { primary: '#F5F8FA', secondary: '#D8E7EF' },
 		success: { main: '#2EB67D' },
 		error: { main: '#E01E5A' },
 		warning: { main: '#ECB22E' },
