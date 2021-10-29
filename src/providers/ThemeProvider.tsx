42eb3dd037387e61763de982e31896575f72d3ab
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-10-29 20:48:16 +0200

    adjusted ThemeProvider to use usePreferredTheme hook

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,10 +1,13 @@
 import React, { PropsWithChildren } from 'react';
 import { CssBaseline, ThemeProvider as MUIThemeProvider } from '@mui/material';
 import { darkTheme } from '../smpUI/smpUITheme';
+import usePreferredTheme from '../hooks/usePreferredTheme';
 
 const ThemeProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+	const { preferredTheme } = usePreferredTheme();
+
 	return (
-		<MUIThemeProvider theme={darkTheme}>
+		<MUIThemeProvider theme={preferredTheme}>
 			<CssBaseline>{children}</CssBaseline>
 		</MUIThemeProvider>
 	);
