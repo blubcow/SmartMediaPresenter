3efb53b1deab2abf8b6f1c98d8f8fefcb3bbc4ee
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-01 15:26:38 +0100

    finalized UI for Login screen

--- src/views/Auth/Login.tsx
+++ src/views/Auth/Login.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Page from '../../smpUI/layout/Page';
+import LoginPaper from '../components/LoginPaper';
+import { Box, Button } from '../../smpUI/components';
+import useStyles from './styles';
+import { Link } from 'react-router-dom';
 
 const Login: React.FC<{}> = () => {
-	return <Page>login page</Page>;
+	const classes = useStyles();
+
+	return (
+		<Page centeredContent>
+			<Box className={classes.container}>
+				<LoginPaper />
+				<Box className={classes.skipLoginButtonContainer}>
+					<Link to='/home' style={{ textDecoration: 'none' }}>
+						<Button color='warning'>skip login</Button>
+					</Link>
+				</Box>
+			</Box>
+		</Page>
+	);
 };
 
 export default Login;
