83a60caceed4565831783128554b757349987c2c
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-01 16:25:01 +0100

    added clickable flag to IBoxProps

--- src/smpUI/components/Box/index.tsx
+++ src/smpUI/components/Box/index.tsx
@@ -1,10 +1,29 @@
 import React, { PropsWithChildren } from 'react';
-import { Box as MUIBox, BoxProps } from '@mui/material';
+import { Box as MUIBox, BoxProps, ButtonBase } from '@mui/material';
 
-interface IBoxProps extends BoxProps {}
+interface IBoxProps extends BoxProps {
+	clickable?: boolean;
+}
 
 const Box: React.FC<PropsWithChildren<IBoxProps>> = (props) => {
-	return <MUIBox {...props}></MUIBox>;
+	const { clickable = false } = props;
+
+	return (
+		<MUIBox {...props}>
+			{clickable && (
+				<ButtonBase
+					style={{
+						height: '100%',
+						width: '100%',
+						position: 'absolute',
+						top: 0,
+						left: 0,
+					}}
+				/>
+			)}
+			{props.children}
+		</MUIBox>
+	);
 };
 
 export default Box;
