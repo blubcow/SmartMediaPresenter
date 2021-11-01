2a962b147ce25ed9f05157d2619febb9764b9141
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-01 15:26:12 +0100

    finalized UI for LoginPaper

--- src/views/components/LoginPaper/index.tsx
+++ src/views/components/LoginPaper/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Box, Paper, Text } from '../../../smpUI/components';
+import { Box, Paper, Text, TextField, Button } from '../../../smpUI/components';
 import useThemedLogo from '../../../hooks/useThemedLogo';
-import useStyles from './styles';
+import useStyles, {
+	useAuthButtonContainerStyles,
+	useTextFieldContainerStyles,
+} from './styles';
 
 const LoginPaper: React.FC<{}> = () => {
 	const logo = useThemedLogo();
@@ -11,9 +14,47 @@ const LoginPaper: React.FC<{}> = () => {
 		<Paper>
 			<Box className={classes.container}>
 				<img src={logo.logo} className={classes.img} />
-				<Text>Login</Text>
+				<Text variant='h4' fontWeight='bold'>
+					Login
+				</Text>
+				<TextFieldContainer />
+				<AuthButtonContainer />
 			</Box>
 		</Paper>
+	);
+};
+
+const TextFieldContainer = () => {
+	const classes = useTextFieldContainerStyles();
+
+	return (
+		<Box className={classes.container}>
+			<Box className={classes.textFieldContainer}>
+				<TextField label='Email' />
+			</Box>
+			<Box className={classes.textFieldContainer}>
+				<TextField label='password' type='password' />
+			</Box>
+		</Box>
+	);
+};
+
+const AuthButtonContainer = () => {
+	const classes = useAuthButtonContainerStyles();
+
+	return (
+		<Box className={classes.container}>
+			<Box className={classes.buttonContainer}>
+				<Button variant='contained' minWidth='200px'>
+					Login
+				</Button>
+			</Box>
+			<Box className={classes.buttonContainer}>
+				<Button color='info' minWidth='200px'>
+					forgot password?
+				</Button>
+			</Box>
+		</Box>
 	);
 };
 
--- src/views/components/LoginPaper/styles.ts
+++ src/views/components/LoginPaper/styles.ts
@@ -16,3 +16,30 @@ export default makeStyles((theme: Theme) =>
 		},
 	})
 );
+
+export const useTextFieldContainerStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		container: {
+			width: '100%',
+		},
+		textFieldContainer: {
+			width: '100%',
+			marginTop: theme.spacing(3),
+		},
+	})
+);
+
+export const useAuthButtonContainerStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		container: {
+			width: '100%',
+			display: 'flex',
+			flexDirection: 'column',
+		},
+		buttonContainer: {
+			marginTop: theme.spacing(2),
+			display: 'flex',
+			justifyContent: 'center',
+		},
+	})
+);
