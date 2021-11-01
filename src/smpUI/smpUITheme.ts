96e2484abe988b48cb25d12103a3cd554f0efa0a
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-01 16:19:00 +0100

    added contrast text color to themes

--- src/smpUI/smpUITheme.ts
+++ src/smpUI/smpUITheme.ts
@@ -1,8 +1,12 @@
-import { createTheme } from '@mui/material';
+import { createTheme, ThemeOptions } from '@mui/material';
+
+const typography: ThemeOptions['typography'] = () => ({
+	fontFamily: 'Montserrat',
+});
 
 export const lightTheme = createTheme({
 	palette: {
-		primary: { main: '#1DA1F2' },
+		primary: { main: '#1DA1F2', contrastText: '#F5F8FA' },
 		secondary: { main: '#405DE6' },
 		background: { default: '#F5F8FA', paper: '#D8E7EF' },
 		text: { primary: '#14171A', secondary: '#282E33' },
@@ -10,11 +14,12 @@ export const lightTheme = createTheme({
 		error: { main: '#E01E5A' },
 		warning: { main: '#ECB22E' },
 	},
+	typography: typography,
 });
 
 export const darkTheme = createTheme({
 	palette: {
-		primary: { main: '#1DA1F2' },
+		primary: { main: '#1DA1F2', contrastText: '#F5F8FA' },
 		secondary: { main: '#405DE6' },
 		background: { default: '#14171A', paper: '#282E33' },
 		text: { primary: '#F5F8FA', secondary: '#D8E7EF' },
@@ -22,4 +27,5 @@ export const darkTheme = createTheme({
 		error: { main: '#E01E5A' },
 		warning: { main: '#ECB22E' },
 	},
+	typography: typography,
 });
