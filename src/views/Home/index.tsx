31b6961b4e62cc46c02901ec33d4458541da0087
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-02 14:29:13 +0100

    built basic UI for home screen

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Page } from '../../smpUI/layout';
 import HomeTopBar from './HomeTopBar';
+import { Box } from '../../smpUI/components';
+import { ProjectsHeaderRow } from '../components/rows';
+import useStyles from './styles';
+import { Divider } from '@mui/material';
 
 const Home: React.FC<{}> = () => {
-	return <Page TopBar={HomeTopBar}>home</Page>;
+	const classes = useStyles();
+
+	return (
+		<Page TopBar={HomeTopBar}>
+			<Box className={classes.container}>
+				<Divider orientation='vertical' />
+				<Box className={classes.rowsContainer}>
+					<ProjectsHeaderRow />
+				</Box>
+				<Divider orientation='vertical' />
+				<Box className={classes.previewContainer}></Box>
+				<Divider orientation='vertical' />
+			</Box>
+		</Page>
+	);
 };
 
 export default Home;
--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -1,6 +1,27 @@
 import { makeStyles, createStyles } from '@mui/styles';
 import { Theme } from '@mui/material';
 
+export default makeStyles((theme: Theme) =>
+	createStyles({
+		container: {
+			width: '100%',
+			height: '100%',
+			display: 'flex',
+		},
+		rowsContainer: {
+			height: '100%',
+			flex: 0.3,
+			display: 'flex',
+			flexDirection: 'column',
+			overflow: 'scroll',
+		},
+		previewContainer: {
+			height: '100%',
+			flex: 0.7,
+		},
+	})
+);
+
 export const useTopBarStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		contaiener: {
