90a3feab80cfa7f5e8bb3ce57447e3907ff409a7
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-05 12:45:25 +0100

    added SlidePreviewRow

---
+++ src/views/components/rows/SlidePreviewRow.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+	MediaLocation,
+	MediaRessource,
+	Slide,
+} from '../../../shared/types/presentation';
+import { Row, Box, Text } from '../../../smpUI/components';
+import { IRowProps } from '../../../smpUI/components/Row';
+
+interface ISlidePreviewRowProps extends IRowProps {
+	slide: Slide;
+	onSelected: (id: number) => void;
+	selected: boolean;
+}
+
+const SlidePreviewRow: React.FC<ISlidePreviewRowProps> = (props) => {
+	const { slide, onSelected, selected } = props;
+	return (
+		<Row
+			{...props}
+			sx={{
+				height: '100px',
+				border: selected ? '2px dashed' : 0,
+				borderColor: 'primary.main',
+			}}
+			onClick={() => {
+				onSelected(slide.id);
+			}}
+		>
+			<PreviewRow height='100%' items={slide.media} />
+		</Row>
+	);
+};
+
+const ImagePreview = (props: { width: string; location?: MediaLocation }) => {
+	const { width, location } = props;
+
+	return (
+		<Box
+			sx={{
+				width: width,
+				aspectRatio: '16/9',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				padding: 0.1,
+			}}
+		>
+			{location?.local || location?.remote ? (
+				<img
+					src={location?.local ?? location?.remote}
+					style={{ height: '100%', width: '100%' }}
+				/>
+			) : (
+				<Box
+					sx={{
+						height: '100%',
+						width: '100%',
+						bgcolor: 'divider',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Text variant='caption'>no media</Text>
+				</Box>
+			)}
+		</Box>
+	);
+};
+
+const PreviewRow = (props: { height: string; items: MediaRessource[] }) => {
+	const { height, items } = props;
+	return (
+		<Box style={{ height: height, display: 'flex', cursor: 'pointer' }}>
+			{items.map((item, i) => (
+				<ImagePreview
+					width={`${100 / items.length}%`}
+					location={items.find((item) => item.id === i)?.location}
+					key={i}
+				/>
+			))}
+		</Box>
+	);
+};
+
+export default SlidePreviewRow;
--- src/views/components/rows/index.tsx
+++ src/views/components/rows/index.tsx
@@ -1,2 +1,3 @@
 export { default as ProjectsHeaderRow } from './ProjectsHeaderRow';
 export { default as SlidesHeaderRow } from './SlidesHeaderRow';
+export { default as SlidePreviewRow } from './SlidePreviewRow';
