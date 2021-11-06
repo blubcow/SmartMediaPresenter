6f83be83fd52ab6e54a2fc25872de127c8bca8b4
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-06 15:06:31 +0100

    added TopBarDisplayingFilename and adjusted EditTopBar to build on top of said component

--- src/views/Edit/EditTopBar.tsx
+++ src/views/Edit/EditTopBar.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import { TopBar } from '../../smpUI/layout';
-import { Box, Text } from '../../smpUI/components';
+import TopBarDisplayingFilename, {
+	ITopBarDisplayingFilenameProps,
+} from '../components/appBars/TopBarDisplayingFilename';
 
-interface IEditTopBarProps {
-	fileName?: string;
-}
+interface IEditTopBarProps extends ITopBarDisplayingFilenameProps {}
 
 const EditTopBar: React.FC<IEditTopBarProps> = (props) => {
 	const { fileName } = props;
 
-	return (
-		<TopBar canGoBack>
-			<Box
-				sx={{
-					width: '100%',
-					height: '100%',
-					display: 'flex',
-					alignItems: 'center',
-					padding: 2,
-				}}
-			>
-				<Text fontWeight='bold' variant='h5' editable minLength={3}>
-					{fileName}
-				</Text>
-			</Box>
-		</TopBar>
-	);
+	return <TopBarDisplayingFilename />;
 };
 
 export default EditTopBar;
---
+++ src/views/components/appBars/TopBarDisplayingFilename.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TopBar } from '../../../smpUI/layout';
+import { Box, Text } from '../../../smpUI/components';
+
+export interface ITopBarDisplayingFilenameProps {
+	fileName?: string;
+	onFilenameChanged?: (filename: string) => void;
+}
+
+const TopBarDisplayingFilename: React.FC<ITopBarDisplayingFilenameProps> = (
+	props
+) => {
+	const { fileName, onFilenameChanged } = props;
+
+	return (
+		<TopBar canGoBack>
+			<Box
+				sx={{
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					alignItems: 'center',
+					padding: 2,
+				}}
+			>
+				// TODO: show error notification on invalid input
+				<Text
+					fontWeight='bold'
+					variant='h5'
+					editable
+					minLength={3}
+					editableTextDidChange={(_, curr) => {
+						if (onFilenameChanged) onFilenameChanged(curr);
+					}}
+				>
+					{fileName}
+				</Text>
+				{props.children}
+			</Box>
+		</TopBar>
+	);
+};
+
+export default TopBarDisplayingFilename;
