825f7667474afec13a4d0263e36f8ea101e72a88
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-08 20:54:29 +0100

    updated QuickCreateTopBar

--- src/views/QuickCreate/QuickCreateTopBar.tsx
+++ src/views/QuickCreate/QuickCreateTopBar.tsx
@@ -4,14 +4,19 @@ import TopBarDisplayingFilename, {
 } from '../components/appBars/TopBarDisplayingFilename';
 import { Box, Button } from '../../smpUI/components';
 
-interface IQuickCreateTopBarProps extends ITopBarDisplayingFilenameProps {}
+interface IQuickCreateTopBarProps extends ITopBarDisplayingFilenameProps {
+	onCreatePresentation: () => void;
+}
 
 const QuickCreateTopBar: React.FC<IQuickCreateTopBarProps> = (props) => {
+	const { onCreatePresentation } = props;
 	return (
 		<TopBarDisplayingFilename {...props}>
 			<Box sx={{ flex: 1, display: 'flex', justifyContent: 'space-between' }}>
 				<Box />
-				<Button variant='contained'>create presentation</Button>
+				<Button variant='contained' onClick={onCreatePresentation}>
+					create presentation
+				</Button>
 			</Box>
 		</TopBarDisplayingFilename>
 	);
