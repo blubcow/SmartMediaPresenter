83144e0b25fb9eff30c5dfc8480e1764b50fd09e
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-09 18:24:09 +0100

    added QuickCreate to export file of views

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,3 +1,5 @@
 export { default as Home } from './Home';
 export { default as Login } from './Auth/Login';
 export { default as Loading } from './Loading';
+export { default as Edit } from './Edit';
+export { default as QuickCreate } from './QuickCreate';
