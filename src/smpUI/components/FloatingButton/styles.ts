80ad5f33c423fdaae79378b6c397af52f1ebdd0e
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-10 09:56:08 +0100

    updated bottom margin for FloatingButtonContainer

--- src/smpUI/components/FloatingButton/styles.ts
+++ src/smpUI/components/FloatingButton/styles.ts
@@ -5,7 +5,7 @@ export default makeStyles((theme: Theme) =>
 	createStyles({
 		container: {
 			position: 'absolute',
-			bottom: theme.spacing(5),
+			bottom: theme.spacing(3),
 			right: theme.spacing(5),
 			display: 'flex',
 			flexDirection: 'row-reverse',
