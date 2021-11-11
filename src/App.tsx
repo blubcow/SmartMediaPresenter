5e024a55805cb6cc4bce28d5e8d44ec843c3f7bf
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-11 20:25:30 +0100

    added I18NProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ThemeProvider from './providers/ThemeProvider';
 import NavigationProvider from './providers/NavigationProvider';
+import I18NProvider from './providers/I18NProvider';
 
 function App() {
 	return (
-		<ThemeProvider>
-			<NavigationProvider />
-		</ThemeProvider>
+		<I18NProvider>
+			<ThemeProvider>
+				<NavigationProvider />
+			</ThemeProvider>
+		</I18NProvider>
 	);
 }
 
