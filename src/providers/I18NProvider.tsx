43f475ffa1f5d570d46b462925ad95f915589054
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-11 20:24:01 +0100

    added I18NProvider

---
+++ src/providers/I18NProvider.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../i18n/i18n';
+
+const I18NProvider: React.FC<PropsWithChildren<{}>> = (props) => {
+	return <I18nextProvider i18n={i18n}>{props.children}</I18nextProvider>;
+};
+
+export default I18NProvider;
