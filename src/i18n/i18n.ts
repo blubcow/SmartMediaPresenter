d1770594063241964f0836498d577177373ca424
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-11 20:16:22 +0100

    added basic i18next setup

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,26 @@
+import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
+import authDE from './translations/de/auth.json';
+import orderingDE from './translations/de/ordering.json';
+import presDE from './translations/de/presentation.json';
+import authEN from './translations/en/auth.json';
+import orderingEN from './translations/en/ordering.json';
+import presEN from './translations/en/presentation.json';
+
+export enum i18nNamespace {
+	Auth = 'auth',
+	Ordering = 'ordering',
+	Presentation = 'presentation',
+}
+
+i18n.use(initReactI18next).init({
+	lng: 'en',
+	fallbackLng: 'en',
+	ns: [i18nNamespace.Auth, i18nNamespace.Ordering, i18nNamespace.Presentation],
+	resources: {
+		en: { ordering: orderingEN, auth: authEN, presentation: presEN },
+		de: { ordering: orderingDE, auth: authDE, presentation: presDE },
+	},
+});
+
+export default i18n;
