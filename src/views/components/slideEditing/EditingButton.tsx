9edff857eaca46f8d80be710b478049ef50bea09
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-16 16:34:46 +0100

    added EditingButton component which will be used for slide editing

---
+++ src/views/components/slideEditing/EditingButton.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react';
+import { Box, Card } from '../../../smpUI/components';
+import { ButtonBase } from '@mui/material';
+import { ICardProps } from '../../../smpUI/components/Card';
+import useStyles from './styles';
+
+export interface IEditingButtonProps extends ICardProps {
+	icon: ReactNode;
+	secondaryNode: ReactNode;
+	selected: boolean;
+}
+
+const EditingButton: React.FC<IEditingButtonProps> = (props) => {
+	const { icon, secondaryNode, selected } = props;
+	const classes = useStyles();
+
+	return (
+		<Card
+			className={classes.card}
+			variant='elevation'
+			elevation={10}
+			sx={{ outlineWidth: selected ? '2px' : 0 }}
+			{...props}
+		>
+			<ButtonBase className={classes.container}>
+				<Box className={classes.iconContainer}>{icon}</Box>
+				<Box className={classes.secondaryContainer}>{secondaryNode}</Box>
+			</ButtonBase>
+		</Card>
+	);
+};
+
+export default EditingButton;
