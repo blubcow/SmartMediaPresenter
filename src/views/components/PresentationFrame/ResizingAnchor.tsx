8126cf6a82864e2423d52e8d36ab665346236e49
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-23 18:23:00 +0100

    added ResizingAnchor and updated PresentationFrame component

---
+++ src/views/components/PresentationFrame/ResizingAnchor.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Box } from '../../../smpUI/components';
+
+type VerticalResizingAnchorPosition = 'top' | 'bottom';
+type HorizontalResizingAnchorPosition = 'left' | 'right';
+export type AnchorPosition = {
+	vertical: VerticalResizingAnchorPosition;
+	horizontal: HorizontalResizingAnchorPosition;
+};
+
+interface IResizingAnchorProps {
+	isVisible: boolean;
+	position: AnchorPosition;
+	onMouseDown: (
+		e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+		pos: AnchorPosition
+	) => void;
+}
+
+const ResizingAnchor: React.FC<IResizingAnchorProps> = (props) => {
+	const { position, isVisible, onMouseDown } = props;
+
+	return (
+		<Box
+			onMouseDown={(e) => {
+				onMouseDown(e, position);
+			}}
+			sx={{
+				display: isVisible ? 'initial' : 'none',
+				width: '15px',
+				height: '15px',
+				position: 'absolute',
+				cursor: 'move',
+				backgroundColor: 'white',
+				outline: '1px solid black',
+				top: position.vertical === 'top' ? '-7.5px' : undefined,
+				left: position.horizontal === 'left' ? '-7.5px' : undefined,
+				right: position.horizontal === 'right' ? '-7.5px' : undefined,
+				bottom: position.vertical === 'bottom' ? '-7.5px' : undefined,
+			}}
+		/>
+	);
+};
+
+export default ResizingAnchor;
--- src/views/components/PresentationFrame/index.tsx
+++ src/views/components/PresentationFrame/index.tsx
@@ -4,29 +4,82 @@ import {
 	PresentationFrameSettings,
 } from '../../../shared/types/presentation';
 import { Box } from '../../../smpUI/components';
+import ResizingAnchor, { AnchorPosition } from './ResizingAnchor';
 
 interface IPresentationFrameProps {
 	isEditing: boolean;
+	isHidden?: boolean;
 	parentSize: Dimensions;
 	settings?: PresentationFrameSettings;
 	outlineColor: string;
-	onSizeChanged?: (size: Dimensions) => void;
+	onPresentationFrameChanged?: (
+		presentationFrame: PresentationFrameSettings
+	) => void;
 }
 
 const PresentationFrame: React.FC<IPresentationFrameProps> = (props) => {
-	const { isEditing, parentSize, settings, outlineColor, onSizeChanged } =
-		props;
+	const {
+		isEditing,
+		isHidden = false,
+		parentSize,
+		settings,
+		outlineColor,
+		onPresentationFrameChanged,
+	} = props;
 	const [heightMultiplier, setHeightMultiplier] = useState<number>(
 		parentSize.height / (settings?.rel.height ?? 1)
 	);
 	const [widthMultiplier, setWidthMultiplier] = useState<number>(
 		parentSize.width / (settings?.rel.width ?? 1)
 	);
+	const [currentSettings, setCurrentSettings] = useState<
+		PresentationFrameSettings | undefined
+	>(settings);
+	const [editingSettings, setEditingSettings] = useState<
+		PresentationFrameSettings | undefined
+	>(settings);
+	const [initialPosition, setInitialPosition] = useState<{
+		x: number;
+		y: number;
+	}>({ x: 0, y: 0 });
+	const [activePosition, setActivePosition] = useState<
+		AnchorPosition | undefined
+	>();
 
 	useEffect(() => {
-		setHeightMultiplier(parentSize.height / (settings?.rel.height ?? 1));
-		setWidthMultiplier(parentSize.width / (settings?.rel.width ?? 1));
-	}, [parentSize, settings?.rel]);
+		setHeightMultiplier(parentSize.height / (editingSettings?.rel.height ?? 1));
+		setWidthMultiplier(parentSize.width / (editingSettings?.rel.width ?? 1));
+	}, [parentSize, editingSettings?.rel]);
+
+	const onDrag = (x: number, y: number) => {
+		if (!activePosition) return;
+		const newSettings: PresentationFrameSettings = {
+			rel: { height: parentSize.height, width: parentSize.width },
+			top: 0,
+			left: 0,
+			right: 0,
+			bottom: 0,
+			...currentSettings,
+		};
+		newSettings.top =
+			activePosition.vertical === 'top'
+				? Math.max(0, newSettings.top - y)
+				: newSettings.top;
+		newSettings.left =
+			activePosition.horizontal === 'left'
+				? Math.max(0, newSettings.left - x)
+				: newSettings.left;
+		newSettings.right =
+			activePosition.horizontal === 'right'
+				? Math.max(0, newSettings.left + x)
+				: newSettings.right;
+		newSettings.bottom =
+			activePosition.vertical === 'bottom'
+				? Math.max(0, newSettings.bottom + y)
+				: newSettings.bottom;
+
+		setEditingSettings({ ...newSettings });
+	};
 
 	return (
 		<Box
@@ -34,16 +87,73 @@ const PresentationFrame: React.FC<IPresentationFrameProps> = (props) => {
 				position: 'absolute',
 				zIndex: 10,
 				outlineStyle: 'solid',
-				outlineWidth: isEditing ? '4px' : `${parentSize.width ?? 0}px`,
+				outlineWidth: isHidden
+					? '0px'
+					: isEditing
+					? '4px'
+					: `${parentSize.width ?? 0}px`,
 				outlineColor: isEditing ? 'yellow' : outlineColor,
 				pointerEvents: isEditing ? 'initial' : 'none',
 				cursor: 'grab',
-				top: `${heightMultiplier * (settings?.top ?? 0)}px`,
-				left: `${widthMultiplier * (settings?.left ?? 0)}px`,
-				height: `${heightMultiplier * (settings?.size.height ?? 1)}px`,
-				width: `${widthMultiplier * (settings?.size.width ?? 1)}px`,
+				top: `${heightMultiplier * (editingSettings?.top ?? 0)}px`,
+				left: `${widthMultiplier * (editingSettings?.left ?? 0)}px`,
+				right: `${widthMultiplier * (editingSettings?.right ?? 0)}px`,
+				bottom: `${widthMultiplier * (editingSettings?.bottom ?? 0)}px`,
 			}}
-		/>
+		>
+			<Box
+				sx={{
+					bgcolor: 'transparent',
+					height: '100%',
+					width: '100%',
+					pointerEvents: isEditing ? 'initial' : 'none',
+					position: 'relative',
+				}}
+				onMouseUp={() => {
+					setActivePosition(undefined);
+					setCurrentSettings(
+						editingSettings ? { ...editingSettings } : undefined
+					);
+				}}
+				onMouseMove={(e) => {
+					if (activePosition)
+						onDrag(initialPosition.x - e.pageX, initialPosition.y - e.pageY);
+				}}
+			>
+				<ResizingAnchor
+					position={{ vertical: 'top', horizontal: 'left' }}
+					isVisible={isEditing}
+					onMouseDown={(e, pos) => {
+						setActivePosition(pos);
+						setInitialPosition({ x: e.pageX, y: e.pageY });
+					}}
+				/>
+				<ResizingAnchor
+					position={{ vertical: 'top', horizontal: 'right' }}
+					isVisible={isEditing}
+					onMouseDown={(e, pos) => {
+						setActivePosition(pos);
+						setInitialPosition({ x: e.pageX, y: e.pageY });
+					}}
+				/>
+				<ResizingAnchor
+					position={{ vertical: 'bottom', horizontal: 'left' }}
+					isVisible={isEditing}
+					onMouseDown={(e, pos) => {
+						setActivePosition(pos);
+						setInitialPosition({ x: e.pageX, y: e.pageY });
+					}}
+				/>
+				<ResizingAnchor
+					position={{ vertical: 'bottom', horizontal: 'right' }}
+					isVisible={isEditing}
+					onMouseDown={(e, pos) => {
+						setActivePosition(pos);
+						setInitialPosition({ x: e.pageX, y: e.pageY });
+					}}
+				/>
+			</Box>
+		</Box>
 	);
 };
 
