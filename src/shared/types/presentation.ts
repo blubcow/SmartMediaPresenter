5732a12442ebb407824dfdf8108b44b87b856e79
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-24 22:11:04 +0100

    added notes to SlideSettings type

--- src/shared/types/presentation.ts
+++ src/shared/types/presentation.ts
@@ -26,6 +26,7 @@ export interface Slide {
 export interface SlideSettings {
 	color?: string;
 	presentationFrame?: PresentationFrameSettings;
+	notes?: string;
 }
 
 export interface MediaRessource {
