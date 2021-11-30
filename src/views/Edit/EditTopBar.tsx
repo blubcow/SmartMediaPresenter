e2b154d2cc447258813ce3a2cbdb7d293164d6dc
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-30 17:33:46 +0100

    changed presentation name will now also be saved

--- src/views/Edit/EditTopBar.tsx
+++ src/views/Edit/EditTopBar.tsx
@@ -7,6 +7,7 @@ import {
 	SlideSettings as SlideSettingsType,
 } from '../../shared/types/presentation';
 import { Box } from '../../smpUI/components';
+import { PresentationEditingActionIdentifiers } from '../../types/identifiers';
 import TopBarDisplayingFilename, {
 	ITopBarDisplayingFilenameProps,
 } from '../components/appBars/TopBarDisplayingFilename';
@@ -16,11 +17,22 @@ import SlideSettings from '../components/slideEditing/SlideSettings';
 interface IEditTopBarProps extends ITopBarDisplayingFilenameProps {}
 
 const EditTopBar: React.FC<IEditTopBarProps> = (props) => {
-	const { state } = usePresentationEditingContext();
-	const { editingControls } = state;
+	const { state, dispatch } = usePresentationEditingContext();
+	const { presentation, editingControls } = state;
 
 	return (
-		<TopBarDisplayingFilename {...props} withFixedHeight={'120px'}>
+		<TopBarDisplayingFilename
+			{...props}
+			withFixedHeight={'120px'}
+			onFilenameChanged={(filename) => {
+				const newPresentation = { ...presentation };
+				newPresentation.name = filename;
+				dispatch({
+					type: PresentationEditingActionIdentifiers.presentationSettingsUpdated,
+					payload: { presentation: newPresentation },
+				});
+			}}
+		>
 			<Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
 				{editingControls === 'media' ? <MediaSettings /> : <SlideSettings />}
 			</Box>
