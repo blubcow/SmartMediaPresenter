773b4e89ca7c7528a3c0acacddd8b4325a864c3c
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-11-30 16:55:29 +0100

    added dispatchMediaTranslationTransformation method to usePresentationEditingContext hook

--- src/hooks/usePresentationEditingContext.ts
+++ src/hooks/usePresentationEditingContext.ts
@@ -1,21 +1,67 @@
+import { access } from 'fs';
 import { useContext, Dispatch } from 'react';
 import { PresentationEditingContext } from '../providers/PresentationEditingProvider';
+import { MediaRessource } from '../shared/types/presentation';
+import { PresentationEditingActionIdentifiers } from '../types/identifiers';
 import {
 	PresentationEditingSettings,
 	PresentationEditingAction,
 } from '../types/state';
 
 const usePresentationEditingContext = () => {
-	const context = useContext(PresentationEditingContext);
+	const context = useContext(PresentationEditingContext) as {
+		state: PresentationEditingSettings;
+		dispatch: Dispatch<PresentationEditingAction>;
+	};
 	if (!context)
 		throw new Error(
 			'usePresentationEditing hook has to be called inside of an PresentationEditingProvider!'
 		);
 
-	return context as {
-		state: PresentationEditingSettings;
-		dispatch: Dispatch<PresentationEditingAction>;
+	const dispatchMediaTranslationTransformation = (x: number, y: number) => {
+		if (context.state.activeMedia === undefined)
+			throw new Error(
+				'tried to translate a media object without a media object being active!'
+			);
+
+		const mediaResource: MediaRessource =
+			context.state.presentation.slides[context.state.currentSlide].media[
+				context.state.activeMedia
+			];
+		const heightDivider =
+			context.state.editingBoxDimensions.height /
+			(mediaResource.settings?.translation?.rel.height ??
+				context.state.editingBoxDimensions.height);
+		const widthDivider =
+			context.state.editingBoxDimensions.width /
+			(mediaResource.settings?.translation?.rel.width ??
+				context.state.editingBoxDimensions.width);
+		const prevTransformation = {
+			x: (mediaResource?.settings?.translation?.x ?? 0) * widthDivider,
+			y: (mediaResource?.settings?.translation?.y ?? 0) * heightDivider,
+		};
+		const currentTransformation = {
+			x: x,
+			y: y,
+		};
+
+		const mediaSettings = { ...mediaResource.settings };
+		mediaSettings.translation = {
+			rel: { ...context.state.editingBoxDimensions },
+			x: (prevTransformation.x ?? 0) + currentTransformation.x,
+			y: (prevTransformation.y ?? 0) - currentTransformation.y,
+		};
+		const newPresentation = { ...context.state.presentation };
+		newPresentation.slides[context.state.currentSlide].media[
+			context.state.activeMedia
+		].settings = mediaSettings;
+		context.dispatch({
+			type: PresentationEditingActionIdentifiers.presentationSettingsUpdated,
+			payload: { presentation: newPresentation },
+		});
 	};
+
+	return { ...context, dispatchMediaTranslationTransformation };
 };
 
 export default usePresentationEditingContext;
