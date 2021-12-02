c90a97fd082aacacd597959748998e3a832c2d9b
Lukas Jäger <a11709111@unet.univie.ac.at>
2021-12-02 15:39:37 +0100

    added Popover to add audio button

--- src/views/components/slideEditing/AudioButton.tsx
+++ src/views/components/slideEditing/AudioButton.tsx
@@ -1,25 +1,77 @@
-import React from 'react';
-import EditingButton, { IEditingButtonProps } from './EditingButton';
+import React, { useState } from 'react';
+import EditingButton from './EditingButton';
+import { Box, Popover } from '../../../smpUI/components';
 import { Audiotrack } from '@mui/icons-material';
 import { useTranslation } from 'react-i18next';
 import { i18nNamespace } from '../../../i18n/i18n';
 import EditButtonLabel from './EditButtonLabel';
+import { ButtonBase, Typography } from '@mui/material';
+import { useAudioButtonStyles } from './styles';
+import { Download, Mic } from '@mui/icons-material';
 
 interface IAudioButtonProps {}
 
 const AudioButton: React.FC<IAudioButtonProps> = (props) => {
 	const { t } = useTranslation([i18nNamespace.Presentation]);
+
+	const [anchorElement, setAnchorElement] = useState<
+		HTMLDivElement | undefined
+	>(undefined);
+	const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		setAnchorElement(e.currentTarget);
+	};
+	const handleClose = () => setAnchorElement(undefined);
+	const classes = useAudioButtonStyles();
+
 	return (
-		<EditingButton
-			icon={
-				<Audiotrack
-					sx={{ color: 'text.primary', height: '100%', width: '100%' }}
+		<>
+			<EditingButton
+				icon={
+					<Audiotrack
+						sx={{ color: 'text.primary', height: '100%', width: '100%' }}
+					/>
+				}
+				secondaryNode={<EditButtonLabel>{t('addAudio')}</EditButtonLabel>}
+				selected={false}
+				onClick={handleClick}
+				{...props}
+			/>
+			<Popover
+				className={classes.popover}
+				open={!!anchorElement}
+				onClose={handleClose}
+				anchorEl={anchorElement}
+			>
+				<OptionRow
+					icon={<Download />}
+					label={t('importAudio')}
+					onClick={() => {}}
 				/>
-			}
-			secondaryNode={<EditButtonLabel>{t('addAudio')}</EditButtonLabel>}
-			selected={false}
-			{...props}
-		/>
+				<OptionRow icon={<Mic />} label={t('recordAudio')} onClick={() => {}} />
+			</Popover>
+		</>
+	);
+};
+
+interface IOptionRowProps {
+	icon: React.ReactNode;
+	label: string;
+	onClick: () => void;
+}
+
+const OptionRow: React.FC<IOptionRowProps> = (props) => {
+	const { icon, label, onClick } = props;
+	const classes = useAudioButtonStyles();
+
+	return (
+		<Box className={classes.rowContainer} onClick={onClick}>
+			<ButtonBase className={classes.btnBase}>
+				<Box className={classes.optionRow}>
+					<Box className={classes.optionRowIcon}>{icon}</Box>
+					<Typography>{label}</Typography>
+				</Box>
+			</ButtonBase>
+		</Box>
 	);
 };
 
--- src/views/components/slideEditing/styles.ts
+++ src/views/components/slideEditing/styles.ts
@@ -105,3 +105,35 @@ export const useMediaEditingModalStyles = makeStyles((theme: Theme) =>
 		},
 	})
 );
+
+export const useAudioButtonStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		popover: {
+			display: 'flex',
+			flexDirection: 'column',
+			minWidth: '250px',
+		},
+		rowContainer: {
+			height: '50px',
+			width: '100%',
+		},
+		btnBase: { width: '100%' },
+		optionRow: {
+			height: '100%',
+			width: '100%',
+			padding: theme.spacing(2),
+			display: 'flex',
+			flexDirection: 'row',
+			alignContent: 'center',
+			justifyContent: 'flex-start',
+			cursor: 'pointer',
+			'&:hover': {
+				backgroundColor: theme.palette.divider,
+			},
+		},
+
+		optionRowIcon: {
+			marginRight: theme.spacing(2),
+		},
+	})
+);
