be18fd461a8cca915fe1487d254c85bc2658b09e
Lukas Jäger <a11709111@unet.univie.ac.at>
2022-01-16 19:56:07 +0100

    added remote namespace to i18n

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -4,16 +4,19 @@ import authDE from './translations/de/auth.json';
 import orderingDE from './translations/de/ordering.json';
 import presDE from './translations/de/presentation.json';
 import alertDE from './translations/de/alert.json';
+import remoteDE from './translations/de/remote.json';
 import authEN from './translations/en/auth.json';
 import orderingEN from './translations/en/ordering.json';
 import presEN from './translations/en/presentation.json';
 import alertEN from './translations/en/alert.json';
+import remoteEN from './translations/en/remote.json';
 
 export enum i18nNamespace {
 	Auth = 'auth',
 	Ordering = 'ordering',
 	Presentation = 'presentation',
 	Alert = 'alert',
+	Remote = 'remote',
 }
 
 export const AvailableLanguages = ['auto', 'en', 'de'] as const;
@@ -22,19 +25,26 @@ export type AvailableLanguage = typeof AvailableLanguages[number];
 i18n.use(initReactI18next).init({
 	lng: navigator.language.split('_')[0],
 	fallbackLng: 'en',
-	ns: [i18nNamespace.Auth, i18nNamespace.Ordering, i18nNamespace.Presentation],
+	ns: [
+		i18nNamespace.Auth,
+		i18nNamespace.Ordering,
+		i18nNamespace.Presentation,
+		i18nNamespace.Remote,
+	],
 	resources: {
 		en: {
 			ordering: orderingEN,
 			auth: authEN,
 			presentation: presEN,
 			alert: alertEN,
+			remote: remoteEN,
 		},
 		de: {
 			ordering: orderingDE,
 			auth: authDE,
 			presentation: presDE,
 			alert: alertDE,
+			remote: remoteDE,
 		},
 	},
 });
